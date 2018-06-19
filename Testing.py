be474103333846e697d9d2611fc219ce0e70dfff
sreekanthsirivella <sreekanth.sirivella@gmail.com>
2018-06-19 17:48:13 +0530

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -2,7 +2,7 @@ import time
 
 for i in range(1,11):
 	time.sleep(0.5)
-	print('Hello Welcome to Jenkins {} Times'.format(str(i)))
+	print('Hello Welcome to Jenkins {} Times',format(str(i)))
 	time.sleep(0.5)
 	print('This is Awesome')
 	time.sleep(0.5)
