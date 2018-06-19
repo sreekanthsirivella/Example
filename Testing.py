75f90aabbeaede87231a2473e47c34e7ee465d8b
sreekanthsirivella <sreekanth.sirivella@gmail.com>
2018-06-19 17:37:23 +0530

    Testing of GIT Repo

---
+++ Testing.py
@@ -0,0 +1,14 @@
+import time
+
+for i in range(1,11):
+	time.sleep(0.5)
+	print('Hello Welcome to Jenkins {} Times'.format(str(i)))
+	time.sleep(0.5)
+	print('This is Awesome')
+	time.sleep(0.5)
+	print('This is Fantastic')
+	time.sleep(0.3)
+	print('This is the First one to trigger')
+	print('hi')
+	
+
