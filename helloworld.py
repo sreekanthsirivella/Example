d39c150443ee2e9fc16fd71ee277a9c4e0b2816c
sreekanthsirivella <sreekanth.sirivella@gmail.com>
2018-06-22 11:13:59 +0530

    Changes done in first program

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,11 @@
+import time
+
 print('Hello world!!!!')
+print('Hi This is the first program from jenkins')
+for i in range(0,100):
+  time.sleep(0.5)
+  print('Loop is in {} iteration '.format(str(i)))
+  print('This is Test Program')
+  
+        
 
