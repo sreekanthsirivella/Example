656482f666dcc8a800db54c4358a188bb6fd2cdf
sreekanth.sirivella <sreekanth.sirivella@gmail.com>
2018-06-25 17:57:30 +0530

    Code modified with changes in for loop.

--- helloworld.py
+++ helloworld.py
@@ -3,7 +3,7 @@ import time
 print('Hello world!!!!')
 print ('Something.......')
 
-for i in range(1,100000):
+for i in range(1,1000):
     time.sleep(1)
     print('Current Position is : {}'.format(str(i)))
     time.sleep(0.5)
